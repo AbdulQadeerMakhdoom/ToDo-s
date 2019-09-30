619ec86939940fcf31795a3a8162c965cc842fad
Abdul Qadeer Makhdoom <abdulqadeer9212@gmail.com>
2019-10-01 02:23:31 +0500

    convert to MVC design Patteren

---
+++ ToDo's/DataModel/item.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  ToDo's
+//
+//  Created by Abdul Qadeer on 01/10/2019.
+//  Copyright © 2019 Abdul Qadeer. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}
